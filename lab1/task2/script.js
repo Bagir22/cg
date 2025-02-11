096fe52f96098cea1b34b6e2e44b7157629f9eca
Bagir22 <bagir2202@gmail.com>
2025-02-11 22:12:24 +0300

    lab1

---
+++ lab1/task2/script.js
@@ -0,0 +1,27 @@
+function dragAndDrop() {
+    let startX = 0, startY = 0, currentX = 0, currentY = 0;
+
+    const trolleybus = document.getElementById('trolleybus');
+
+    trolleybus.addEventListener('mousedown', (e) => {
+        startX = e.clientX - currentX;
+        startY = e.clientY - currentY;
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    });
+
+    function onMouseMove(e) {
+        currentX = e.clientX - startX;
+        currentY = e.clientY - startY;
+
+        trolleybus.style.transform = `translate(${currentX}px, ${currentY}px)`;
+    }
+
+    function onMouseUp() {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+}
+
+dragAndDrop();
